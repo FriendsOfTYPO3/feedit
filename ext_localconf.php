a4d92d0c5e467c83c4a5c6470a2d1fb89fbad320
Christian Kuhn <lolli@schwarzbu.ch>
2019-01-20 12:55:15 +0100

    [TASK] Add feedit specific code from ext:adminpanel

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,3 +3,8 @@ defined('TYPO3_MODE') or die();
 
 // Register the edit panel view.
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/classes/class.frontendedit.php']['edit'] = \TYPO3\CMS\Feedit\FrontendEditPanel::class;
+
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['adminpanel']['modules']['edit'] = [
+    'module' => \TYPO3\CMS\Feedit\Modules\EditModule::class,
+    'after' => ['cache'],
+];
